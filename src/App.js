f04f662b02ba4fe07c589f463290eb9025c5bf87
Aja Singer <ajasinger@gmail.com>
2023-02-18 12:03:55 -0500

    move components into src folder & import to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 //import logo from './logo.svg';
 //import './App.css';
+import Title from './components/TitleBlock';
+import QuestionBlock from './components/QuestionBlock';
+import QuestionsBlock from './components/QuestionsBlock';
+import AnswerBlock from './components/AnswerBlock';
 
 const App = () => {
   return (
