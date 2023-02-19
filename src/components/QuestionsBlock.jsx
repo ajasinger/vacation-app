024e2a95bd77b378468e11ccba56a5d6553f2166
Aja Singer <ajasinger@gmail.com>
2023-02-19 14:36:57 -0500

    add classNames and id

--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -3,7 +3,9 @@ const QuestionsBlock = ({quizItem}) => {
     
     return (
         <>
-            <h2>{quizItem.text}</h2>
+            {/* add id to scroll to each question */}
+            <h2 id={quizItem.id} className="question-title">{quizItem.text}</h2>
+            <div className="questions-container"></div>
         </>
     )
 }
