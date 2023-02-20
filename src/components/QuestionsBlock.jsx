1fbaaacd79a6234643279a451e432ec8207ea3d7
Aja Singer <ajasinger@gmail.com>
2023-02-19 21:53:47 -0500

    map quizItem.questions

--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,11 +1,19 @@
+import QuestionBlock from './QuestionBlock';
+
 const QuestionsBlock = ({quizItem}) => {
     // console.log(quizItem);
     
     return (
         <>
             {/* add id to scroll to each question */}
+            {/* why is this not displaying ?????????????????????? */}
             <h2 id={quizItem.id} className="question-title">{quizItem.text}</h2>
             <div className="questions-container"></div>
+            {/* map quizItem.question to display each question option */}
+            {quizItem.questions.map((question, index) => {
+                <QuestionBlock key={index} question={question}/>
+            })
+            }
         </>
     )
 }
