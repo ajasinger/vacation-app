2800c8e629aa8ed509315c9a1dab7d60e178e3d9
Aja Singer <ajasinger@gmail.com>
2023-02-19 22:07:22 -0500

    return button individual question option

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,8 +1,15 @@
-const QuestionBlock = () => {
+const QuestionBlock = ({question}) => {
     return (
-        <div>
-
-        </div>
+        // make entire question option a button containing image, text, links 
+        <button
+            className="question-block"
+        >
+            <img src={question.image} alt={question.alt}/>
+            <h3>{question.text}</h3>
+            <p>
+                <a href="https://www.unsplash.com">{question.credit}</a>
+            </p>
+        </button>
     );
 }
 
