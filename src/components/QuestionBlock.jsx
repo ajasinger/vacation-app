3c67f9e98ff8728bb29e919e100e5e0b4da58ebb
Aja Singer <ajasinger@gmail.com>
2023-02-20 18:59:54 -0500

    use setAnswers to create answers object

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -3,13 +3,18 @@ import QuizContext from "../QuizContext";
 
 const QuestionBlock = ({id, question}) => {
 
-    //define variable setAnswers 
+    //define setAnswers with useContext() hook
+    //???????
     const { setAnswers } = useContext(QuizContext);
 
     return (
         // make entire question option a button containing image, text, links 
         <button
             className="question-block"
+            //on click new answer is added to existing answers object with an id
+            onClick={
+                () => setAnswers((answers) => ({...answers,[id]: question.text}))
+            }
         >
             <img src={question.image} alt={question.alt}/>
             <h3>{question.text}</h3>
