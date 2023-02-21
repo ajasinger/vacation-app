d76713f333c66f7f5d6a8ed9062fa3c8ee7d7fd3
Aja Singer <ajasinger@gmail.com>
2023-02-21 09:34:37 -0500

    update notes

--- src/QuizContext.js
+++ src/QuizContext.js
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
-//use createContext to pass answer object to all children
 const QuizContext= createContext({
     answers: {},
     setAnswers: () => {}
 });
+
 export default QuizContext;
