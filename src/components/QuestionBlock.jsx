1403279095682668d1c8c842de9e17809e678139
Aja Singer <ajasinger@gmail.com>
2023-02-21 10:04:28 -0500

    update notes

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -3,12 +3,10 @@ import QuizContext from "../QuizContext";
 
 const QuestionBlock = ({id, question}) => {
 
-    //define setAnswers with useContext() hook
-    //???????
+    //destructure QuizContext to pull out setAnswers
     const { setAnswers } = useContext(QuizContext);
 
     return (
-        // make entire question option a button containing image, text, links 
         <button
             className="question-block"
             //on click new answer is added to existing answers object with an id
@@ -19,10 +17,8 @@ const QuestionBlock = ({id, question}) => {
             <img src={question.image} alt={question.alt}/>
             <h3>{question.text}</h3>
             <p>
-                {/* remove tabbing option inside of button with tabIndex */}
                 <a tabIndex="-1" href="https://www.unsplash.com">{question.credit}</a>
             </p>
-            
         </button>
     );
 }
